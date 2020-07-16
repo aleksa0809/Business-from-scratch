d2ba105dd42e3669165384f05f1d5b9374af8cc8
mabel <js@yababay.ru>
2020-07-16 10:42:34 +0300

    page-structure

--- app.md
+++ app.md
@@ -9,4 +9,22 @@ First of all, it is needed to choose a css-framework for web page appearence. Th
     <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 ```
 
+The next step is creating of a structure of the page. There are `Navbar` and `LeftMenu` components on the top and left sides on page respectively:
+
+```
+<div>  
+    <Navbar />
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-3">
+                <LeftMenu />
+            </div>
+            <div class="col-lg-9">
+                <p>Here will be pages.</p>
+            </div>
+        </div>
+    </div>
+</div>
+```
+
 
--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import Navbar from "./components/navbar"
+import LeftMenu from "./components/leftmenu"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    render() {
+      return  (
+        <div>  
+            <Navbar />
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-3">
+                        <LeftMenu />
+                    </div>
+                    <div class="col-lg-9">
+                        <p>Here will be pages.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        )
+    }
 }
 
 export default App;
